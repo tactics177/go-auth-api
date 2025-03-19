abc06ab4a1e26e61515c35ccb3c8e765b0a5632d
tactics177 <63974953+tactics177@users.noreply.github.com>
2025-03-19 17:12:47 +0100

    added health check endpoint

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/73f2fc18-3053-4c38-943a-416d16432450/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "API is running"})
+	})
+
+	port := "8080"
+	fmt.Println("Server running on port " + port)
+	router.Run(":" + port)
+}
